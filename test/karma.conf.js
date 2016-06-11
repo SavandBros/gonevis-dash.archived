159822679a9645841ad4e265fbbfc8154e160d11
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-06-11 16:07:22 +0400

    Update tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,8 +29,9 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-material/angular-material.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
