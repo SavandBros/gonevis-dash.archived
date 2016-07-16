73bf5032728d3159dc7c60b1240ae9d197c36d89
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-07-16 21:36:06 +0400

    Update header component

---
+++ app/header/header_controller.js
@@ -0,0 +1,20 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name gonevisDash.controller:HeaderController
+ * Controller of the gonevisDash
+ */
+function HeaderController($scope) {
+    /**
+     * constructor
+     *
+     * @method constructor
+     * @desc Init function for controller
+     *
+     * @memberOf HeaderController
+     */
+    function constructor() {}
+}
+
+app.controller("HeaderController", HeaderController);
+HeaderController.$inject = ['$scope'];
