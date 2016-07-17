5c46dc0fa32b22afa6161d0a5ce818f4dd380f00
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-07-17 19:58:49 +0400

    Update signin

--- app/signin/signin_controller.js
+++ app/signin/signin_controller.js
@@ -6,11 +6,12 @@
  * Controller of the gonevisDash
  * 
  * @param $scope
+ * @param $rootScope
  * @param $state
  * @param $mdToast
  * @param AuthenticationService
  */
-function SigninController($scope, $state, $mdToast, AuthenticationService) {
+function SigninController($scope, $rootScope, $state, $mdToast, AuthenticationService) {
 
     // Signin form
     $scope.form = {};
@@ -24,13 +25,13 @@ function SigninController($scope, $state, $mdToast, AuthenticationService) {
      * @memberOf SigninController
      */
     function constructor() {
-        
+
         // Check auth
         if (AuthenticationService.isAuthenticated()) {
             $state.go('main');
         }
     };
-    
+
     /**
      * signin
      *
@@ -39,11 +40,27 @@ function SigninController($scope, $state, $mdToast, AuthenticationService) {
      *
      */
     $scope.signin = function (form) {
-        AuthenticationService.login(form.email, form.password);
+        form.loading = true;
+
+        AuthenticationService.login(form.username, form.password).then(
+            function (data, status, headers, config) {
+                AuthenticationService.setAuthenticatedUser(data.data.user);
+                AuthenticationService.setToken(data.data.token);
+
+                $rootScope.$broadcast('gonevisDash.account.service.AuthenticationService:Authenticated');
+                $mdToast.showSimple('Welcome ' + data.data.user.username);
+                $state.go('main');
+            },
+            function (data, status, headers, config) {
+                form.loading = false;
+                form.error = true;
+                form.errors = data.data;
+            }
+        );
     }
 
     constructor();
 }
 
 app.controller("SigninController", SigninController);
-SigninController.$inject = ['$scope', '$state', '$mdToast', 'AuthenticationService'];
+SigninController.$inject = ['$scope', '$rootScope', '$state', '$mdToast', 'AuthenticationService'];
