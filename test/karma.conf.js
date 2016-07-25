cae459bc45fd03811e5be0b853eacfd9a920c50f
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-07-25 23:35:38 +0400

    Karma for deps

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-gettext/dist/angular-gettext.js',
       'bower_components/angular-ui-router-title/angular-ui-router-title.js',
+      'bower_components/angular-xeditable/dist/js/xeditable.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/configuration.js',
