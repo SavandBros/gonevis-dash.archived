82ef5f7939d71af1aca74d9de63a638b0268ddf5
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-07-31 05:41:36 +0400

    Update routers to have dash and childrens

--- app/app.js
+++ app/app.js
@@ -8,30 +8,43 @@
  *
  * Main module of the application.
  */
-app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
     $httpProvider.interceptors.push('AuthenticationInterceptorService');
 
     $stateProvider
-        .state('main', {
+        // Every child of dash state follows site id that user is into
+        .state('dash', {
+            url: '/:siteId',
+            abstract: true,
+            controller: 'DashController',
+            template: '<ui-view/>'
+        })
+        .state('dash.main', {
             url: '/',
             controller: 'MainController',
             templateUrl: 'main/main_view.html'
         })
-        .state('entry-new', {
+        .state('dash.entry-new', {
             url: '/new',
             controller: 'EntryNewController',
             templateUrl: 'entry_new/entry_new_view.html'
         })
-        .state('entry-list', {
-            url: '/site/:siteId',
+        .state('dash.entry-list', {
+            url: '/site',
             controller: 'EntryListController',
             templateUrl: 'entry_list/entry_list.html'
         })
-        .state('entry-edit', {
-            url: '/site/:siteId/:entryId',
+        .state('dash.entry-edit', {
+            url: '/site/:entryId',
             controller: 'EntryEditController',
             templateUrl: 'entry_edit/entry_edit_view.html'
         })
+        .state('dash.user', {
+            url: '/user',
+            controller: 'UserController',
+            templateUrl: 'user/user_view.html'
+        })
+        // Other states that are not a child of dash state
         .state('signin', {
             url: '/login',
             controller: 'SigninController',
@@ -41,20 +54,10 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
             url: '/register',
             controller: 'SignupController',
             templateUrl: 'signup/signup_view.html'
-        })
-        .state('sites', {
-            url: '/site',
-            controller: 'SitesController',
-            templateUrl: 'sites/sites.html'
-        })
-        .state('user', {
-            url: '/user',
-            controller: 'UserController',
-            templateUrl: 'user/user_view.html'
         });
     $urlRouterProvider.otherwise('/');
 });
 
-app.run(function(editableOptions) {
+app.run(function (editableOptions) {
     editableOptions.theme = 'bs3';
 });
