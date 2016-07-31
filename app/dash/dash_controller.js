4eb28635972a4734e228e78289e615472a3d6dfe
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-07-31 05:41:18 +0400

    Add dash

---
+++ app/dash/dash_controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gonevisDash.controller:DashController
+ * Controller of the gonevisDash
+ *
+ * @param $scope
+ * @param $state
+ * @param $mdToast
+ * @param AuthenticationService
+ */
+function DashController($scope, $state, $mdToast, AuthenticationService) {
+    /**
+     * constructor
+     *
+     * @method constructor
+     * @desc Init function for controlleru
+     */
+    function constructor() {
+        // Check auth
+        if (!AuthenticationService.isAuthenticated()) {
+            $mdToast.showSimple('Please login to continue.');
+            $state.go('signin');
+        }
+    }
+
+    constructor();
+}
+
+app.controller("DashController", DashController);
+DashController.$inject = ['$scope', '$state', '$mdToast', 'AuthenticationService'];
