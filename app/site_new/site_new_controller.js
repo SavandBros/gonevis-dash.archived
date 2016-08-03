95aea9d584ccad386cfc1e4e1ec288b61e64f863
Arsalan Savand <arsalan@savandbros.com>
2016-08-03 17:53:00 +0400

    Add site-new component

---
+++ app/site_new/site_new_controller.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gonevisDash.controller:SiteNewController
+ * Controller of the gonevisDash
+ *
+ * @param $scope
+ * @param $rootScope
+ * @param $state
+ * @param $mdToast
+ * @param AuthenticationService
+ */
+function SiteNewController($scope, $rootScope, $state, $mdToast, SiteNewService, AuthenticationService) {
+
+    // Create Site form
+    $scope.form = {};
+
+
+    /**
+     * constructor
+     *
+     * @method constructor
+     * @desc Init function for controller
+     */
+    function constructor() {
+        $scope.user = AuthenticationService.getAuthenticatedUser();
+    };
+
+    $scope.createSite = function(form) {
+        form.loading = true;
+
+        SiteNewService.post(form).then(
+            function(data, status, headers, config) {
+                form.loading = false;
+                // Update sites
+                $scope.user.sites.push(data.data);
+
+                // Update current user's data
+                console.log(AuthenticationService.updateAuthentication($scope.user));
+
+                // Show success message
+                $mdToast.showSimple('Site ' + data.data.title + ' created');
+
+                // Redirect user to the site that just have been created
+                $state.go('dash.entry-new', {s: $scope.user.sites.length -1});
+            },
+            function(data, status, headers, config) {
+                console.log(data.data);
+            }
+        );
+    };
+
+    constructor();
+}
+
+app.controller("SiteNewController", SiteNewController);
+SiteNewController.$inject = ['$scope', '$rootScope', '$state', '$mdToast', 'SiteNewService', 'AuthenticationService'];
