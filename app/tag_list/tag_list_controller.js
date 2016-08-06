537fbdb5b03aaf85f9b6f6afa94177beb785a960
Arsalan Savand <arsalan@savandbros.com>
2016-08-06 16:19:52 +0400

    Add tag list

---
+++ app/tag_list/tag_list_controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gonevisDash.controller:TagListController
+ * Controller of the gonevisDash
+ *
+ * @param $scope
+ * @param $rootScope
+ * @param $state
+ * @param $mdToast
+ * @param TagListService
+ * @param AuthenticationService
+ */
+function TagListController($scope, $rootScope, $state, $mdToast, TagListService, AuthenticationService) {
+
+    var site = AuthenticationService.getCurrentSite();
+
+    /**
+     * constructor
+     *
+     * @method constructor
+     * @desc Init function for controller
+     */
+    function constructor() {
+        $scope.user = AuthenticationService.getAuthenticatedUser();
+
+        TagListService.get(site).then(
+            function(data, status, headers, config) {
+                $scope.tags = data.data.results;
+            },
+            function(data, status, headers, config) {
+                console.log(data.data);
+            }
+        );
+    };
+
+    constructor();
+}
+
+app.controller("TagListController", TagListController);
+TagListController.$inject = ['$scope', '$rootScope', '$state', '$mdToast', 'TagListService', 'AuthenticationService'];
