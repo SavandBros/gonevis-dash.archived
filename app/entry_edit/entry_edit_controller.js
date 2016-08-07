3c1d8464206a4477d9a2aea726c4655500ff0ecd
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-08-07 18:10:06 +0400

    Clean up and update entry edit

--- app/entry_edit/entry_edit_controller.js
+++ app/entry_edit/entry_edit_controller.js
@@ -6,25 +6,14 @@
  * Controller of the gonevisDash
  *
  * @param $scope
- * @param $rootScope
  * @param $state
  * @param $stateParams
  * @param $mdToast
  * @param EntryEditService
  * @param AuthenticationService
  */
-function EntryEditController($scope, $rootScope, $state, $stateParams, $mdToast, EntryEditService, AuthenticationService) {
+function EntryEditController($scope, $state, $stateParams, $mdToast, EntryEditService, AuthenticationService) {
 
-  var s = $stateParams.s
-  var site = AuthenticationService.getCurrentSite();
-
-  // Entry data
-  $scope.form = {
-    id: $stateParams.entryId
-  };
-
-  // Status data
-  $scope.statuses = [{ name: "Draft", id: 0 }, { name: "Hidden", id: 1 }, { name: "Published", id: 2 }];
   /**
    * constructor
    *
@@ -32,85 +21,72 @@ function EntryEditController($scope, $rootScope, $state, $stateParams, $mdToast,
    * @desc Init function for controller
    */
   function constructor() {
-    // Get Entry data
-    EntryEditService.get($scope.form.id).then(
-      function (data, status, headers, config) {
-        $scope.form = data.data;
-        console.log(data.data);
-      },
-      function (data, status, headers, config) {
-        console.log(data.data);
-      }
-    );
+    $scope.form = {
+      id: $stateParams.entryId,
+      site: AuthenticationService.getCurrentSite()
+    };
+
+    $scope.statuses = [
+      { name: "Draft", id: 0 },
+      { name: "Hidden", id: 1 },
+      { name: "Published", id: 2 }
+    ];
+
+    EntryEditService.get($scope.form.id).then(function (data) {
+      $scope.form = data.data;
+    })
   }
 
 
   /**
-   * updateEntry
+   * update
    *
-   * @method updateEntry
-   * @desc Submit updateEntry form
+   * @method update
+   * @desc Update entry API callback
    * 
    * @param form {object}
    */
-  $scope.updateEntry = function (form) {
+  $scope.update = function (form) {
     form.loading = true;
-    // Api call for updating entry
+
     EntryEditService.put(form).then(
-      function (data, status, headers, config) {
-        // Succsess message
-        $mdToast.showSimple("Entry successfully updated !");
+      function (data) {
+        $mdToast.showSimple("Entry updated!");
         form.loading = false;
+        form.errors = null;
       },
-      function (data, status, headers, config) {
-        // Failure message
-        $mdToast.showSimple("Sorry, couldn't update entry");
+      function (data) {
+        $mdToast.showSimple("Couldn't update entry!");
         form.loading = false;
+        form.errors = data.data;
       }
     );
   }
 
   /**
-   * deleteEntry
+   * delete
    *
-   * @method deleteEntry
+   * @method delete
    * @desc delete entry via api call
    * 
-   * @param entryId {string}
+   * @param id {string} UUID of entry
    */
-  $scope.deleteEntry = function (entryId) {
-    EntryEditService.del(entryId).then(
-      function (data, status, headers, config) {
+  $scope.delete = function (id) {
+    EntryEditService.del(id).then(
+      function (data) {
         $mdToast.showSimple("Entry has been deleted !");
         $state.go('dash.entry-list');
       },
-      function (data, status, headers, config) {
-        console.log(data.data);
+      function (data) {
+        $mdToast.showSimple("Something went wrong... We couldn't delete entry!");
       }
     );
   };
-
-  /* Need to be fixed
-  $scope.search = function(query) {
-      EntryEditService.search(site).then(
-          function(data, status, headers, config) {
-              $scope.result = data.data.results;
-              console.log($scope.result);
-          },
-          function(data, status, headers, config) {
-              console.log(data.data);
-          }
-      );
-  }
-  */
-  // check user auth
-  $scope.$on('gonevisDash.AuthenticationService:Authenticated', function () {
-    constructor();
-    $state.go('main');
-  });
 
   constructor()
 }
 
 app.controller('EntryEditController', EntryEditController)
-EntryEditController.$inject = ['$scope', '$rootScope', '$state', '$stateParams', '$mdToast', 'EntryEditService', 'AuthenticationService']
+EntryEditController.$inject = [
+  '$scope', '$state', '$stateParams', '$mdToast', 'EntryEditService', 'AuthenticationService'
+]
