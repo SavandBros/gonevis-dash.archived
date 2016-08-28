9eeaa47d5546a50106dffb03ed14e0c552b70152
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-08-28 22:03:36 +0400

    Add accepted file types

--- app/dolphin/dolphin_controller.js
+++ app/dolphin/dolphin_controller.js
@@ -37,7 +37,53 @@ function DolphinController($scope, $state, $stateParams, $mdToast, API, ENV, Aut
     );
 
     $scope.upload = {
+      file: null,
+      done: false,
+      accept: "",
+      acceptList: [
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png',
+        'image/gif',
+        'application/pdf',
+        'application/msword',
+        'application/x-iwork-keynote-sffkey',
+        'application/mspowerpoint',
+        'application/powerpoint',
+        'application/vnd.ms-powerpoint',
+        'application/x-mspowerpoint',
+        'application/vnd.oasis.opendocument.text',
+        'application/excel',
+        'application/vnd.ms-excel',
+        'application/x-excel',
+        'application/x-msexcel',
+        'application/x-compressed',
+        'application/x-zip-compressed',
+        'application/zip',
+        'multipart/x-zip',
+        'audio/mpeg3',
+        'audio/x-mpeg-3',
+        'video/mpeg',
+        'video/x-mpeg',
+        'audio/x-m4a',
+        'audio/ogg',
+        'audio/wav',
+        'audio/x-wav',
+        'video/mp4',
+        'video/x-m4v',
+        'video/quicktime',
+        'video/x-ms-wmv',
+        'video/avi',
+        'video/msvideo',
+        'video/x-msvideo',
+        'video/mpeg',
+        'video/ogg',
+        'video/3gp',
+        'video/3gpp2',
+      ]
     }
+
+    $scope.upload.accept = $scope.upload.acceptList.join(',');
   }
 
   /**
