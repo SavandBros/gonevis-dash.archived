6fa2afa33b7347ed56f977321e9de3b6f662c4de
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-09-09 02:00:42 +0400

    Fix and Update modal service

---
+++ app/basement/shared/modals/modals_service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * API Service
+ *
+ * @returns {{Service: API.service}}
+ */
+function ModalsService(ModalService) {
+
+  var templates = {
+    dolphin: "basement/modals/dolphin/dolphin_view.html"
+  };
+
+  function open(template, controller, data) {
+    ModalService.showModal({
+      templateUrl: templates[template],
+      controller: controller,
+      inputs: data
+    }).then(function (modal) {
+      modal.element.modal();
+    });
+  };
+
+  return {
+    open: open
+  };
+}
+
+app.factory('ModalsService', ModalsService);
+ModalsService.$inject = ['ModalService'];
