78f634dbe53a853aaae98fbc5331a2d32ef35491
Arsalan Savand <arsalan@savandbros.com>
2016-09-11 03:59:21 +0400

    Fix comment modal on comment list controller

--- app/comment/comment_list/comment_list_controller.js
+++ app/comment/comment_list/comment_list_controller.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * @ngdoc function
@@ -13,7 +13,7 @@
  * @param AuthService
  * @param CommentService
  */
-function CommentListController($scope, $rootScope, $state, $mdToast, API, AuthService, CommentService) {
+function CommentListController ($scope, $rootScope, $state, $mdToast, API, AuthService, CommentService) {
 
   /**
    * constructor
@@ -21,25 +21,33 @@ function CommentListController($scope, $rootScope, $state, $mdToast, API, AuthSe
    * @method constructor
    * @desc Init function for controller
    */
-  function constructor() {
-    $scope.user = AuthService.getAuthenticatedUser();
-    $scope.site = AuthService.getCurrentSite();
-    $scope.commentService = CommentService;
+  function constructor () {
+    $scope.user = AuthService.getAuthenticatedUser()
+    $scope.site = AuthService.getCurrentSite()
+    $scope.commentService = CommentService
 
     API.Comments.get({ site_id: $scope.site, object_type: CommentService.objectType },
       function (data, status, headers, config) {
-        $scope.comments = data.results;
+        $scope.comments = data.results
       },
       function (data, status, headers, config) {
-        console.log(data);
+        console.log(data)
       }
-    );
-  };
+    )
+  }
 
-  constructor();
+  $rootScope.$on('gonevisDash.CommentService:delete', function (event, data) {
+    for (var i = 0; i < $scope.comments.length; i++) {
+      if ($scope.comments[i].id == data.id) {
+        $scope.comments[i].isDeleted = true
+      }
+    }
+  })
+
+  constructor()
 }
 
-app.controller("CommentListController", CommentListController);
+app.controller('CommentListController', CommentListController)
 CommentListController.$inject = [
   '$scope', '$rootScope', '$state', '$mdToast', 'API', 'AuthService', 'CommentService'
-];
+]
