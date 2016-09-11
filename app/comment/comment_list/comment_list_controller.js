f4a80d8044af54de573d4e236aec546521c9869c
Arsalan Savand <arsalan@savandbros.com>
2016-09-11 04:21:23 +0400

    Update format

--- app/comment/comment_list/comment_list_controller.js
+++ app/comment/comment_list/comment_list_controller.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /**
  * @ngdoc function
@@ -13,7 +13,7 @@
  * @param AuthService
  * @param CommentService
  */
-function CommentListController ($scope, $rootScope, $state, $mdToast, API, AuthService, CommentService) {
+function CommentListController($scope, $rootScope, $state, $mdToast, API, AuthService, CommentService) {
 
   /**
    * constructor
@@ -21,30 +21,27 @@ function CommentListController ($scope, $rootScope, $state, $mdToast, API, AuthS
    * @method constructor
    * @desc Init function for controller
    */
-  function constructor () {
-    $scope.user = AuthService.getAuthenticatedUser()
-    $scope.site = AuthService.getCurrentSite()
-    $scope.commentService = CommentService
+  function constructor() {
+    $scope.user = AuthService.getAuthenticatedUser();
+    $scope.site = AuthService.getCurrentSite();
+    $scope.commentService = CommentService;
 
     API.Comments.get({ site_id: $scope.site, object_type: CommentService.objectType },
       function (data, status, headers, config) {
-        $scope.comments = data.results
-      },
-      function (data, status, headers, config) {
-        console.log(data)
+        $scope.comments = data.results;
       }
     )
-  }
+  };
 
   $rootScope.$on('gonevisDash.CommentService:delete', function (event, data) {
     for (var i = 0; i < $scope.comments.length; i++) {
       if ($scope.comments[i].id == data.id) {
-        $scope.comments[i].isDeleted = true
+        $scope.comments[i].isDeleted = true;
       }
     }
-  })
+  });
 
-  constructor()
+  constructor();
 }
 
 app.controller('CommentListController', CommentListController)
