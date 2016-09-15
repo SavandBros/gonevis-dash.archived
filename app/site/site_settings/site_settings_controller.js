8a4b9223166d2995a75412fe3943e9ffd280fa34
Arsalan Savand <arsalan@savandbros.com>
2016-09-15 18:49:04 +0400

    Update user data after site successfully deleted

--- app/site/site_settings/site_settings_controller.js
+++ app/site/site_settings/site_settings_controller.js
@@ -71,11 +71,14 @@ function SiteSettingsController($scope, $rootScope, $state, $mdToast, API, AuthS
   $scope.remove = function (siteId) {
     API.SiteUpdate.delete({ site_id: site },
       function (data, status, headers, config) {
+        for (var i = 0; i < $scope.user.sites.length; i++) {
+          if ($scope.user.sites[i].id == site) {
+            $scope.user.sites.splice(i, 1);
+          }
+        };
+        AuthService.updateAuth($scope.user);
+        $rootScope.$broadcast('gonevisDash.SiteSettingsController:delete');
         $mdToast.showSimple("Site deleted");
-
-        $rootScope.$broadcast('gonevisDash.SiteSettingsController:delete', site);
-
-        $state.go('dash.main', { s: $scope.user.sites.length - 2 });
       },
       function (data, status, headers, config) {
         $mdToast.showSimple("Sorry we couldn't delete the site, please try again later");
