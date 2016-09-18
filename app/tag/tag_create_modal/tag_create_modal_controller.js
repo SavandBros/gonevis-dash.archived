5e8c93b173b02a27cf71944cce6f41d0b09e5c4d
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-09-18 19:43:31 +0400

    Fix and Update tag create modal controller

--- app/tag/tag_create_modal/tag_create_modal_controller.js
+++ app/tag/tag_create_modal/tag_create_modal_controller.js
@@ -7,10 +7,14 @@
  *
  * @param $scope
  * @param $rootScope
- * @param TagService
- * @param $element
+ * @param $mdToast
+ * @param API
+ * @param AuthService
+ * @param ModalsService
  */
-function TagCreateModalController($scope, $rootScope, TagService, $element) {
+function TagCreateModalController($scope, $rootScope, $mdToast, API, AuthService, ModalsService) {
+
+  var site = AuthService.getCurrentSite();
 
   /**
    * constructor
@@ -19,14 +23,52 @@ function TagCreateModalController($scope, $rootScope, TagService, $element) {
    * @desc Init function for controller
    */
   function constructor() {
-    $scope.tagService = TagService;
+    $scope.form = {}
   };
 
-  $rootScope.$on('gonevisDash.TagService:create', function () {
-    $element.modal('hide');
-  });
+  /**
+   * create
+   *
+   * @method create
+   * @desc Create a new tag
+   *
+   * @param form {Object}
+   */
+  $scope.create = function (form) {
+    form.loading = true;
+    form.errors = null;
+
+    form.data.slug = form.data.slug ? form.data.slug : "";
+
+    API.Tags.save({ tag_site: site }, form.data,
+      function (data) {
+        form.loading = false;
+
+        $rootScope.$broadcast("gonevisDash.API:create", {
+          success: true,
+          data: data,
+          tag: form.data
+        });
+
+        $mdToast.showSimple("Tag " + data.name + " created.");
+        ModalsService.close('tagCreate');
+      },
+      function (data) {
+        form.loading = false;
+        form.errors = data.data;
+      }
+    );
+  }
+
   constructor();
 }
 
 app.controller("TagCreateModalController", TagCreateModalController);
-TagCreateModalController.$inject = ['$scope', '$rootScope', 'TagService', '$element'];
+TagCreateModalController.$inject = [
+  '$scope',
+  '$rootScope',
+  '$mdToast',
+  'API',
+  'AuthService',
+  'ModalsService'
+];
