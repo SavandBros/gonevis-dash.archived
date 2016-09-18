eabca6b7afc003fb5fc63c2468f8ea8017e53519
Alireza Savand <alireza.savand@gmail.com>
2016-09-18 20:35:57 +0400

    Don't fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,8 +145,7 @@ module.exports = function (grunt) {
     // Make sure code styles are up to par
     jscs: {
       options: {
-        config: ".jscsrc",
-        fix: true
+        config: ".jscsrc"
       },
       all: {
         src: [
