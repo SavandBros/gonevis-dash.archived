a4c5f6519e67db4e92858361ff8914a563ae54cb
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-09-18 19:43:16 +0400

    Fix and Update tag controller

--- app/tag/tag_controller.js
+++ app/tag/tag_controller.js
@@ -69,7 +69,7 @@ function TagController($scope, $rootScope, $state, $mdToast, TagService, API, Au
     );
   }
 
-  $rootScope.$on('gonevisDash.TagService:delete', function (event, data) {
+  $rootScope.$on('gonevisDash.TagService:remove', function (event, data) {
     for (var i = 0; i < $scope.tags.length; i++) {
       if ($scope.tags[i].id == data.id) {
         $scope.tags[i].isDeleted = true;
