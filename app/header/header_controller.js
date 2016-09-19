9d364f429e61a6dd38dba539901cc8668b291709
Arsalan Savand <arsalan@savandbros.com>
2016-09-19 04:41:03 +0400

    Fix header controller code style

--- app/header/header_controller.js
+++ app/header/header_controller.js
@@ -24,13 +24,13 @@ function HeaderController($scope, $rootScope, $state, $stateParams, AuthService)
     $scope.user = AuthService.getAuthenticatedUser();
 
     $scope.state = $state;
-    $scope.param = $stateParams
-  };
+    $scope.param = $stateParams;
+  }
 
   $rootScope.$on("gonevisDash.AuthService:Authenticated", function () {
     constructor();
 
-    if ($scope.user.sites.length == 0) {
+    if ($scope.user.sites.length === 0) {
       $state.go("site-new");
     } else {
       $state.go("dash.main", { s: 0 });
@@ -38,7 +38,7 @@ function HeaderController($scope, $rootScope, $state, $stateParams, AuthService)
   });
 
   $scope.$on("gonevisDash.SiteNewController:Create", function () {
-    constructor()
+    constructor();
   });
 
   $scope.$on("gonevisDash.AuthService:SignedOut", function () {
@@ -46,12 +46,12 @@ function HeaderController($scope, $rootScope, $state, $stateParams, AuthService)
     $state.go("signin");
   });
 
-  $scope.$on("gonevisDash.SiteSettingsController:remove", function (event, id) {
+  $scope.$on("gonevisDash.SiteSettingsController:remove", function () {
     constructor();
   });
 
   constructor();
-};
+}
 
 app.controller("HeaderController", HeaderController);
 HeaderController.$inject = [
