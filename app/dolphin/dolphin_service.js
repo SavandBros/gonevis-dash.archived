20b0f8a27d3595822f66c2cd1d632967f76da0f8
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-09-26 03:36:33 +0400

    Add remove to dolphin service
    Delete a dolphin and broadcast it

--- app/dolphin/dolphin_service.js
+++ app/dolphin/dolphin_service.js
@@ -14,6 +14,33 @@
  * @returns [Factory]
  */
 function DolphinService($rootScope, $mdToast, API, ModalsService) {
+  function remove(dolphin, toast) {
+
+    var toast = toast || true;
+
+    API.Dolphin.remove({ site_id: dolphin.site, file_id: dolphin.id },
+      function (data) {
+        $scope.isDeleted = true;
+        $rootScope.$broadcast('gonevisDash.DolphinService:remove', {
+          dolphin: data,
+          success: true
+        });
+        if (toast) {
+          $mdToast.showSimple("Deleted " + meta_data.name);
+        };
+      },
+      function (data) {
+        $rootScope.$broadcast('gonevisDash.DolphinService:remove', {
+          dolphin: data,
+          success: false
+        });
+        if (toast) {
+          $mdToast.showSimple("Sorry, file couldn't be deleted. Try again later.");
+        };
+      }
+    )
+  };
+
   return {
   };
 }
