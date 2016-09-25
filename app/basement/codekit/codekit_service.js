c65c320d3bd1a0376916620ccfbe83f9c7c11fee
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-09-26 03:34:45 +0400

    Add Codekit service
    * @desc Codekit service is a basement filled with useful function and
    constant data,
    *       It stops repeating code such as functions, static variables,
    etc...
    *       Using this provider is highly recommended, even got a short
    name to be used quickly :P

---
+++ app/basement/codekit/codekit_service.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * Codekit
+ *
+ * @class Codekit
+ * @namespace gonevisDash.Codekit
+ * @desc Codekit service is a basement filled with useful function and constant data,
+ *       It stops repeating code such as functions, static variables, etc...
+ *       Using this provider is highly recommended, even got a short name to be used quickly :P
+ *
+ * @returns [Factory]
+ */
+function Codekit() {
+  return {
+  };
+};
+
+app.factory("Codekit", Codekit);
