b18a00536cd257106199cfed9932d3fd9e3adbab
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-09-27 05:57:47 +0400

    Update dolphin controller code formatter

--- app/dolphin/dolphin_controller.js
+++ app/dolphin/dolphin_controller.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 
 /**
  * @ngdoc function
@@ -27,11 +27,12 @@ function DolphinController($scope, $rootScope, $state, $stateParams, $mdToast,
    * @desc Init function for controller
    */
   function constructor() {
-    $scope.nothing = {
-      text: "It's lonely here... Try adding some dolphins!"
-    };
-
+    $scope.nothing = { text: "It's lonely here... Try adding some dolphins!" };
     $scope.dolphinService = DolphinService;
+
+    if ($rootScope.selectionMode) {
+      $scope.currentTab = 'dolphin';
+    }
 
     API.Dolphins.get({},
       function (data) {
@@ -43,48 +44,48 @@ function DolphinController($scope, $rootScope, $state, $stateParams, $mdToast,
       files: [],
       accept: "",
       acceptList: [
-        'image/jpeg',
-        'image/pjpeg',
-        'image/png',
-        'image/gif',
-        'application/pdf',
-        'application/msword',
-        'application/x-iwork-keynote-sffkey',
-        'application/mspowerpoint',
-        'application/powerpoint',
-        'application/vnd.ms-powerpoint',
-        'application/x-mspowerpoint',
-        'application/vnd.oasis.opendocument.text',
-        'application/excel',
-        'application/vnd.ms-excel',
-        'application/x-excel',
-        'application/x-msexcel',
-        'application/x-compressed',
-        'application/x-zip-compressed',
-        'application/zip',
-        'multipart/x-zip',
-        'audio/mpeg3',
-        'audio/x-mpeg-3',
-        'video/mpeg',
-        'video/x-mpeg',
-        'audio/x-m4a',
-        'audio/ogg',
-        'audio/wav',
-        'audio/x-wav',
-        'video/mp4',
-        'video/x-m4v',
-        'video/quicktime',
-        'video/x-ms-wmv',
-        'video/avi',
-        'video/msvideo',
-        'video/x-msvideo',
-        'video/mpeg',
-        'video/ogg',
-        'video/3gp',
-        'video/3gpp2',
+        "image/jpeg",
+        "image/pjpeg",
+        "image/png",
+        "image/gif",
+        "application/pdf",
+        "application/msword",
+        "application/x-iwork-keynote-sffkey",
+        "application/mspowerpoint",
+        "application/powerpoint",
+        "application/vnd.ms-powerpoint",
+        "application/x-mspowerpoint",
+        "application/vnd.oasis.opendocument.text",
+        "application/excel",
+        "application/vnd.ms-excel",
+        "application/x-excel",
+        "application/x-msexcel",
+        "application/x-compressed",
+        "application/x-zip-compressed",
+        "application/zip",
+        "multipart/x-zip",
+        "audio/mpeg3",
+        "audio/x-mpeg-3",
+        "video/mpeg",
+        "video/x-mpeg",
+        "audio/x-m4a",
+        "audio/ogg",
+        "audio/wav",
+        "audio/x-wav",
+        "video/mp4",
+        "video/x-m4v",
+        "video/quicktime",
+        "video/x-ms-wmv",
+        "video/avi",
+        "video/msvideo",
+        "video/x-msvideo",
+        "video/mpeg",
+        "video/ogg",
+        "video/3gp",
+        "video/3gpp2",
       ]
     };
-    $scope.upload.accept = $scope.upload.acceptList.join(',');
+    $scope.upload.accept = $scope.upload.acceptList.join(",");
   };
 
   /**
@@ -103,7 +104,7 @@ function DolphinController($scope, $rootScope, $state, $stateParams, $mdToast,
     angular.forEach($scope.upload.files,
       function (file) {
         file.upload = Upload.upload({
-          url: ENV.apiEndpoint + 'dolphin/file/',
+          url: ENV.apiEndpoint + "dolphin/file/",
           data: { file: file, site: site }
         });
 
@@ -138,7 +139,7 @@ function DolphinController($scope, $rootScope, $state, $stateParams, $mdToast,
    * @param event {Object}
    * @param data {Object}
    */
-  function update (event, data) {
+  function update(event, data) {
     if (data.success) {
       var index = Codekit.getIndex($scope.dolphins, data.dolphin);
 
@@ -148,8 +149,6 @@ function DolphinController($scope, $rootScope, $state, $stateParams, $mdToast,
     };
   };
 
-  $scope.$on('gonevisDash.DolphinService:update', update);
-  $scope.$on('gonevisDash.DolphinService:remove', update);
   /**
    * action
    *
@@ -165,21 +164,23 @@ function DolphinController($scope, $rootScope, $state, $stateParams, $mdToast,
     DolphinService.view(dolphin);
   };
 
+  $scope.$on("gonevisDash.DolphinService:update", update);
+  $scope.$on("gonevisDash.DolphinService:remove", update);
 
   constructor();
 }
 
-app.controller('DolphinController', DolphinController)
+app.controller("DolphinController", DolphinController)
 DolphinController.$inject = [
-  '$scope',
-  '$rootScope',
-  '$state',
-  '$stateParams',
-  '$mdToast',
-  'DolphinService',
-  'Codekit',
-  'API',
-  'ENV',
-  'AuthService',
-  'Upload'
+  "$scope",
+  "$rootScope",
+  "$state",
+  "$stateParams",
+  "$mdToast",
+  "DolphinService",
+  "Codekit",
+  "API",
+  "ENV",
+  "AuthService",
+  "Upload"
 ]
