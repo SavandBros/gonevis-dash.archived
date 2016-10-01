27450615128dbdbed572ac0f0bad8ee55e029935
Arsalan Savand <arsalan@savandbros.com>
2016-10-01 16:31:06 +0400

    Add cover image selection for TagModalController

--- app/tag/tag_modal/tag_modal_controller.js
+++ app/tag/tag_modal/tag_modal_controller.js
@@ -9,7 +9,7 @@
  * @param TagService
  * @param tag
  */
-function TagModalController($scope, TagService, tag) {
+function TagModalController($scope, TagService, tag, DolphinService) {
 
   /**
    * constructor
@@ -19,11 +19,21 @@ function TagModalController($scope, TagService, tag) {
    */
   function constructor() {
     $scope.tagService = TagService;
+    $scope.dolphinService = DolphinService;
     $scope.tag = tag;
   };
+
+  $scope.$on("gonevisDash.DolphinService:select", function (data, dolphin) {
+    $scope.tag.cover_image = dolphin.id;
+  });
 
   constructor();
 }
 
 app.controller("TagModalController", TagModalController);
-TagModalController.$inject = ['$scope', 'TagService', 'tag'];
+TagModalController.$inject = [
+  "$scope",
+  "TagService",
+  "tag",
+  "DolphinService"
+];
