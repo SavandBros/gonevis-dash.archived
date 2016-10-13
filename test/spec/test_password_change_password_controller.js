5aa3e6f4a706639e6178b30e717b73cd280aed83
Arsalan Savand <arsalan@savandbros.com>
2016-10-13 22:15:28 +0400

    Add new tests for change password controller

---
+++ test/spec/test_password_change_password_controller.js
@@ -0,0 +1,51 @@
+describe("ChangePasswordController", function () {
+  beforeEach(
+    module("gonevisDash")
+  );
+
+  var $controller;
+
+  beforeEach(inject(function (_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  describe("$scope.changePassword", function () {
+    beforeEach(function () {
+      $scope = {};
+      controller = $controller('ChangePasswordController', { $scope: $scope });
+    });
+
+    it("is a new password", function () {
+      var form = {
+        old_password: 'oldalireza',
+        password: 'oldalireza',
+      };
+      var result = $scope.changePassword(form);
+      expect(result).toEqual({ non_field_errors: ['Please, choose a new password.'] });
+    });
+
+    it("check if Confirm new password and new password were matched, if so raise an error", function () {
+      var form = {
+        old_password: 'oldalireza',
+        password: 'newPassword',
+        confirm_password: 'newPassword1'
+      };
+      var result = $scope.changePassword(form);
+
+      expect(result).toEqual({ non_field_errors: ["Confirm password does not match."] });
+    });
+
+    it("change password successfully", function () {
+      var form = {
+        old_password: 'oldalireza',
+        password: 'newPassword',
+        confirm_password: 'newPassword'
+      };
+      var result = $scope.changePassword(form);
+
+      expect(result).toBe(undefined);
+      expect(form.errors).toBe(null);
+      expect(form.loading).toBe(true);
+    });
+  });
+});
