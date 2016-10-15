edbec755b42f93a64c0339af44addc5e62860b82
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-10-15 17:43:07 +0400

    Add documentation for Search

--- app/basement/search/search_service.js
+++ app/basement/search/search_service.js
@@ -1,4 +1,17 @@
 "use strict";
+
+/**
+ * Search
+ *
+ * @class Search
+ * @namespace gonevisDash.Search
+ *
+ * @param $rootScope
+ * @param $resource
+ * @param Pagination
+ *
+ * @returns [Factory]
+ */
 function Search($rootScope, $resource, Pagination) {
 }
 
