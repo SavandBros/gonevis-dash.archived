c3908d1b954169f8d24672bb64b59f6a7cf3063d
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-10-15 23:45:36 +0400

    More testing for search service

--- test/spec/basement/search/search_service.js
+++ test/spec/basement/search/search_service.js
@@ -2,22 +2,39 @@
 
 describe("search service", function () {
   var search;
+  var $httpBackend;
+  var apiToQuery;
 
   beforeEach(module("gonevisDash"));
 
-  beforeEach(inject(function (_Search_) {
+  beforeEach(inject(function (_Search_, _$httpBackend_, _$resource_) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/search').respond({ results: ["item"] });
     search = _Search_;
+    apiToQuery = _$resource_('/search');
   }));
 
-  it("should searchify and have no result", function () {
+  it("should searchify without result", function () {
     var searchForm = search.searchify(searchForm, {}, {}, {}, {});
     expect(searchForm.noResult).toEqual(true);
   });
 
-  it("should searchify and have result", function () {
+  it("should searchify with result", function () {
     var data = { results: ["item"] };
     var searchForm = search.searchify(searchForm, {}, {}, data, {});
     expect(searchForm.noResult).toEqual(false);
     expect(searchForm.data.results.length).toEqual(1);
+  });
+
+  it("should submit query with results", function () {
+    var searchForm = search.searchify(searchForm, {}, apiToQuery.get, {}, {});
+    search.submit(searchForm);
+    $httpBackend.flush();
+
+    expect(searchForm.noResult).toEqual(false);
+    expect(searchForm.data.results.length).toEqual(1);
+
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   });
 });
