32462eb63ad553694c287834be3ec554bc47bb62
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-10-17 01:18:31 +0400

    Ignore app_config.js

--- app/app_config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- var app = angular.module('gonevisDash', ['ngMaterial', 'ngAnimate', 'ui.router', 'ngAnimate', 'ngCookies', 'ngMessages', 'ngResource', 'ngSanitize', 'gettext', 'xeditable', 'textAngular', 'angularModalService', 'ngTagsInput', 'ngFileUpload', 'slugifier', 'angular-sortable-view'])
-
-.constant('ENV', {googleAnalyticsID:'UA-XXXXXXX-X',name:'development',apiEndpoint:'http://127.0.0.1:8000/api/v1/'})
-
-;
