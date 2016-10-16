e1a3715192d7127b1e615ee8b0863a7056b6e490
Amir Mehdi Savnad <amirm3hdi@gmail.com>
2016-10-16 19:38:23 +0400

    Check authentication at app.run

--- app/app_run.js
+++ app/app_run.js
@@ -1,9 +1,16 @@
 "use strict";
 
-app.run(function ($rootScope, editableOptions, ModalsService) {
+app.run(function ($rootScope, $mdToast, editableOptions, ModalsService, AuthService) {
   editableOptions.theme = "bs3";
 
   $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+    // Check authentication
+    if (toState.auth === true && !AuthService.isAuthenticated() ||
+      toState.auth === false && AuthService.isAuthenticated()) {
+      event.preventDefault();
+    };
+
+    // Check current site
     if (!toParams.s) {
       ModalsService.open("sites");
     };
