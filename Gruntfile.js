bb72de304cf78dbded3d2c3892521ff07acc0f9f
Amir Mehdi Savand <amir@savandbros.com>
2016-11-12 14:54:38 +0400

    Fix html live reload level

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
           livereload: "<%= connect.options.livereload %>"
         },
         files: [
-          "<%= gonevisDash.app %>/*/{,*/,*/}*.html",
+          "<%= gonevisDash.app %>/*/{,*/}/{,*/}*.html",
           ".tmp/styles/{,*/}*.css",
           "<%= gonevisDash.app %>/assets/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}"
         ]
