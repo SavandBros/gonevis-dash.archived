a2498f48904f0196a73b4df86a41f542ec625531
Amir Mehdi Savand <amir@savandbros.com>
2016-11-12 14:58:30 +0400

    Add ResetPassController

---
+++ app/account/reset_pass/reset_pass_controller.js
@@ -0,0 +1,11 @@
+"use strict";
+function ResetPassController($scope, $state, AuthService, API) {
+}
+
+app.controller("ResetPassController", ResetPassController);
+ResetPassController.$inject = [
+  "$scope",
+  "$state",
+  "AuthService",
+  "API"
+];
