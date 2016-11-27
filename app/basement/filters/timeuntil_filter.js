a8e8b355a9587bc97af3f9c816260c71d22d96d6
Amir Mehdi Savand <amir@savandbros.com>
2016-11-27 17:31:26 +0400

    Update format and documentation of time until filter

--- app/basement/filters/timeuntil_filter.js
+++ app/basement/filters/timeuntil_filter.js
@@ -1,18 +1,19 @@
-/*global app*/
-'use strict';
-
+"use strict";
 
 /**
- * Time Until Filter
  * @class TimeUntilFilter
- * @namespace timeuntil
+ *
+ * @param $filter
+ *
  * @returns {Function}
  */
 function TimeUntilFilter($filter) {
-  return function(date) {
-    return $filter('timesince')(date, true);
+  return function (date) {
+    return $filter("timesince")(date, true);
   }
 }
 
-app.filter('timeuntil', TimeUntilFilter);
-TimeUntilFilter.$inject = ['$filter'];
+app.filter("timeuntil", TimeUntilFilter);
+TimeUntilFilter.$inject = [
+  "$filter"
+];
