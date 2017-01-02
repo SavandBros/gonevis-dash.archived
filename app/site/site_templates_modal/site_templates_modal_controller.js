da4923c7b559a0b8e8c41e582ae1e7c768b21d8a
Arsalan Savand <arsalan@savandbros.com>
2017-01-02 21:37:38 +0400

    Site templates modal controller
    That get the list of available templates and chooses one

---
+++ app/site/site_templates_modal/site_templates_modal_controller.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * @class SiteTemplatesModalController
+ *
+ * @param $scope
+ * @param $rootScope
+ * @param API
+ * @param AuthService
+ */
+function SiteTemplatesModalController($scope, $rootScope, API, AuthService) {
+
+  var site = AuthService.getCurrentSite();
+
+  /**
+   * @method constructor
+   * @desc Init function for controller
+   */
+  function constructor() {
+
+    API.SiteTemplates.get({site_id: site},
+      function (data) {
+        $scope.siteTemplates = data.templates;
+      }
+    );
+  }
+
+  $scope.setTemplate = function(template) {
+    $rootScope.$broadcast("gonevisDash.SiteTemplatesModalController:setTemplate", {
+      data: template
+    });
+  };
+
+  constructor();
+}
+
+app.controller("SiteTemplatesModalController", SiteTemplatesModalController);
+SiteTemplatesModalController.$inject = [
+  "$scope",
+  "$rootScope",
+  "API",
+  "AuthService"
+];
