c5f0e764e4b9d04a38459c30c31b51d9b435e6a5
Alireza Savand <alireza.savand@gmail.com>
2017-02-06 04:34:06 +0400

    More files for rsync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 grunt release
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $DEPLOY_USER@$DEPLOY_HOST -p$DEPLOY_SSH_PORT 'rm -rf $DEPLOY_PATH*'
-rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p$DEPLOY_SSH_PORT" --progress dist/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p$DEPLOY_SSH_PORT" --progress .htaccess dist/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
