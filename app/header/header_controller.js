cfee53b67305099d5b858f5cb4570e6d724d8cd5
Amir Mehdi Savand <amir@savandbros.com>
2017-04-01 18:34:30 +0400

    Add documentation to events

--- app/header/header_controller.js
+++ app/header/header_controller.js
@@ -25,6 +25,10 @@ function HeaderController($scope, $rootScope, $state, $stateParams, toaster, Aut
   }
 
   $rootScope.$on("gonevisDash.AuthService:Authenticated", function () {
+  /**
+   * @event gonevisDash.AuthService:Authenticated
+   * @desc Authentication loads
+   */
   $scope.$on("gonevisDash.AuthService:Authenticated", function () {
     constructor();
     if (!$scope.user.sites.length) {
@@ -34,6 +38,13 @@ function HeaderController($scope, $rootScope, $state, $stateParams, toaster, Aut
     }
   });
 
+  /**
+   * @event gonevisDash.AuthService:SignedOut
+   * @desc Un-authentication redirect
+   *
+   * @param event {Event}
+   * @param sessionExpired {Boolean}
+   */
   $scope.$on("gonevisDash.AuthService:SignedOut", function (event, sessionExpired) {
     if (sessionExpired) {
       toaster.info("Logged out", "Looks like your session has expired, login again.");
@@ -42,11 +53,28 @@ function HeaderController($scope, $rootScope, $state, $stateParams, toaster, Aut
     $state.go("signin");
   });
 
+  /**
+   * @event gonevisDash.SiteNewController:Create
+   * @desc Site creation load
+   */
+  $scope.$on("gonevisDash.SiteNewController:Create", constructor);
 
+  /**
+   * @event gonevisDash.SiteController:remove
+   * @desc Site removal
+   */
   $scope.$on("gonevisDash.SiteController:remove", constructor);
 
+  /**
+   * @event gonevisDash.SiteController:update
+   * @desc Site update
+   */
   $scope.$on("gonevisDash.SiteController:update", constructor);
 
+  /**
+   * @event gonevisDash.UserController:update
+   * @desc User update
+   */
   $scope.$on("gonevisDash.UserController:update", constructor);
 
   constructor();
