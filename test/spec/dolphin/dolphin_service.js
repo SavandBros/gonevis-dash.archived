776f8ac42e17f8972481f9fceea6c211938e9246
Arsalan Savand <arsalan@savandbros.com>
2017-04-04 17:50:02 +0400

    Add dolphin service unit test

---
+++ test/spec/dolphin/dolphin_service.js
@@ -0,0 +1,24 @@
+"use strict";
+/* global AuthService */
+
+describe("Dolphin", function () {
+  beforeEach(module("gonevisDash"));
+
+  var Dolphin, AuthService;
+  var data = { ext: "application/zip" };
+
+  beforeEach(inject(function (_Dolphin_, _AuthService_) {
+    Dolphin = _Dolphin_;
+    AuthService = _AuthService_;
+    AuthService.getCurrentSite = function () {
+      return [];
+    };
+  }));
+
+  it("constructs", function () {
+    var dolphin = new Dolphin(data);
+
+    expect(dolphin.extension).toEqual("ZIP");
+    expect(dolphin.isDeleted).not.toBeTruthy();
+  });
+});
