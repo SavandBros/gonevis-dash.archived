a342dd266fabfba1a6df8170eda4a251c4d640f3
Amir Mehdi Savand <amir@savandbros.com>
2017-04-11 16:52:20 +0400

    Remove Dolphin API

--- app/api/api_service.js
+++ app/api/api_service.js
@@ -124,9 +124,6 @@ function API($http, $resource, ENV) {
     {
       name: "Dolphins",
       endpoint: "dolphin/file/" // GET, POST
-    }, {
-      name: "Dolphin",
-      endpoint: "dolphin/file/:fileId/" // GET, PUT, DELETE
     }
   ];
 
