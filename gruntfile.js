85102d1b02142b94eb17595382678f22dcc02a12
Amir Mehdi Savand <amir@savandbros.com>
2017-06-18 16:40:22 +0400

    Add comment for quick switch later

--- gruntfile.js
+++ gruntfile.js
@@ -77,6 +77,7 @@ module.exports = function (grunt) {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: "localhost",
+        // hostname: "0.0.0.0",
         livereload: 35729
       },
       livereload: {
