d3bf865bce10a5ec8a8c6ea135c4e0fe2361743c
Arsalan Savand <arsalan@savandbros.com>
2017-07-04 23:30:01 +0400

    Fix documentation in site new controller

--- app/site/site_new/site_new_controller.js
+++ app/site/site_new/site_new_controller.js
@@ -8,8 +8,8 @@
  * @param $state
  * @param $stateParams
  * @param API
- * @param toaster
  * @param AuthService
+ * @param toaster
  */
 function SiteNewController($scope, $rootScope, $state, $stateParams, API, AuthService, toaster) {
 
