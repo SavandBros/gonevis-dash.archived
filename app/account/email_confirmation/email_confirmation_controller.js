2f916b51b445104712fc24f4fcf1372c1acd0893
Amir Mehdi Savand <amir@savandbros.com>
2017-07-25 16:35:02 +0400

    Update email confirmation controller to support modal

--- app/account/email_confirmation/email_confirmation_controller.js
+++ app/account/email_confirmation/email_confirmation_controller.js
@@ -3,34 +3,46 @@
 function EmailConfirmationController($scope, $rootScope, $state, toaster, API, AuthService) {
 
   function constructor() {
-    $scope.loading = true;
-    API.EmailConfirmation.save({}, { token: $state.params.token },
-      function (data) {
-        $scope.loading = false;
-        toaster.success("Done", "Thanks for confirming your email");
+    // If not logged in, attempt to confirm with given token
+    if (!AuthService.isAuthenticated()) {
+      $scope.loading = true;
 
-        AuthService.setAuthenticatedUser(data.user);
-        AuthService.setToken(data.token);
+      API.EmailConfirmation.save({}, { token: $state.params.token },
+        function (data) {
+          $scope.loading = false;
+          toaster.success("Done", "Thanks for verifying your email.");
 
-        $rootScope.$broadcast("gonevisDash.AuthService:Authenticated");
-      },
-      function () {
-        $scope.loading = false;
-        $scope.error = true;
-      }
-    );
+          AuthService.setAuthenticatedUser(data.user);
+          AuthService.setToken(data.token);
+
+          $rootScope.$broadcast("gonevisDash.AuthService:Authenticated");
+        },
+        function () {
+          $scope.loading = false;
+          $scope.error = true;
+        }
+      );
+    }
+
+    // Logged in, wants to resend email confirmation link
+    else {
+      $scope.user = AuthService.getAuthenticatedUser(true);
+      $scope.form = {
+        email: $scope.user.get.email
+      };
+    }
   }
 
   /**
-   * @desc Resend email confirmation
+   * @desc Resend email confirmation link
    *
    * @param {object} form
    */
   $scope.resend = function (form) {
     API.EmailConfirmationResend.save({ email: form.email },
       function () {
-        toaster.success("Sent", "Email confirmation sent to " + form.email);
-        $state.go('signin');
+        toaster.success("Sent", "Email verification sent to " + form.email);
+        $state.go("signin");
       },
       function (data) {
         form.errors = data.data;
