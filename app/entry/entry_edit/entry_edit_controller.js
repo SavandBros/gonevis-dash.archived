28e2900417095bef8ad74fb973084a571e98a737
Amir Savand <amir@savandbros.com>
2017-09-10 18:55:17 +0400

    Stick toolbar to top of screen but under navbar

--- app/entry/entry_edit/entry_edit_controller.js
+++ app/entry/entry_edit/entry_edit_controller.js
@@ -159,6 +159,20 @@ function EntryEditController($scope, $rootScope, $state, $stateParams, $timeout,
     }
   });
 
+  /**
+   * @desc Stick toolbar to top of screen but under navbar
+   */
+  angular.element(window).scroll(function () {
+    var topOffset = window.scrollY;
+    var maxOffset = angular.element(".navbar-static-top .container").height() + 2;
+    var offset = -maxOffset;
+    // If not passed navbar height
+    if (topOffset < maxOffset) {
+      offset = -topOffset;
+    }
+    angular.element(".static-toolbar").animate({ marginTop: offset }, 0);
+  });
+
   constructor();
 }
 
