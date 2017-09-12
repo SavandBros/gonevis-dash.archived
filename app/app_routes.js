7e77d0c0be87fcaf7c7bcc1e51dcae948fb2faff
Amir Savand <amir@savandbros.com>
2017-09-12 20:32:11 +0400

    Add editor property to views with editor

--- app/app_routes.js
+++ app/app_routes.js
@@ -148,6 +148,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
       templateUrl: "entry/entry_new/entry_new_view.html",
       auth: true,
       clickEvent: true,
+      editor: true,
       params: {
         lights: true,
       },
@@ -159,6 +160,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
       templateUrl: "entry/entry_edit/entry_edit_view.html",
       auth: true,
       clickEvent: true,
+      editor: true,
       params: {
         entryId: null,
         lights: true
