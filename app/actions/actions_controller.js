e9cb48e189f172e5b726873f50dc8d52a7bd1117
Arsalan Savand <arsalan@savandbros.com>
2017-11-01 18:54:21 +0400

    Add documentation

--- app/actions/actions_controller.js
+++ app/actions/actions_controller.js
@@ -6,10 +6,17 @@ function ActionsController($scope, $state, $stateParams, AuthService) {
     $scope.params = $stateParams;
     $scope.user = AuthService.getAuthenticatedUser(true);
 
+    // We have action and param
     if ($scope.params.action && $scope.params.actionParam) {
+
+      // Dashboard
       if ($scope.params.action === "dashboard") {
+
+        // Find site by given id
         angular.forEach($scope.user.getSites(), function(site, index) {
           if ($scope.params.actionParam === site.id) {
+
+            // Go to site dashboard
             $state.go("dash.main", {
               s: index
             });
