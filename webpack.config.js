942fe92b045c96a61a2bbc5acfdd7f459fcc2925
Alireza Savand <alireza.savand@gmail.com>
2018-06-02 15:48:14 +0300

    Remove DedupePlugin plugin

--- webpack.config.js
+++ webpack.config.js
@@ -197,11 +197,6 @@ module.exports = function makeWebpackConfig() {
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
       // Only emit files when there are no errors
       new webpack.NoErrorsPlugin(),
-
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-      // Dedupe modules in the output
-      new webpack.optimize.DedupePlugin(),
-
       // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
       // Minify all javascript, switch loaders to minimizing mode
       new webpack.optimize.UglifyJsPlugin(),
