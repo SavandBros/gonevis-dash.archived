7a3c3736afcfb2b515656a99cfd4b3953469c8ee
Alireza Savand <alireza.savand@gmail.com>
2018-06-02 15:42:22 +0300

    Load all the required CSS files

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,23 @@ require('./app_config');
 require('./app_routes');
 require('./app_run');
 require('./app');
+
+// Styles!
+import 'jquery'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
+import 'angular-xeditable/dist/css/xeditable.css'
+import 'ng-tags-input/build/ng-tags-input.css'
+import 'animate.css/animate.css'
+import 'medium-editor/dist/css/medium-editor.css'
+import 'medium-editor/dist/css/themes/default.css'
+
+// Our own styles <3
+import '../style/css/main.css'
+import '../style/css/ice.css'
+import '../style/css/checkbox.css'
+import '../style/css/shorties.css'
+
 // Application
 require('./api/api_service');
 require('./modals/modals_service');
