cb0c0a1d744e77210dd7a24cb5469a291374ad24
Alireza Savand <alireza.savand@gmail.com>
2018-06-09 13:55:36 +0300

    Add tag form component

--- src/app/tag/index.js
+++ src/app/tag/index.js
@@ -1,4 +1,5 @@
 require("./tag_controller");
 require("./tag_service");
+require("./tag_form_component");
 require("./tag_modal/tag_modal_controller");
 require("./tag_new_modal/tag_new_modal_controller");
