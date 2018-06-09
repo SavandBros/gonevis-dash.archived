14ab415668f29d68aea69a26fdb6ec28312d5db3
Arsalan <arsalan@savandbros.com>
2018-06-09 20:49:59 +0300

    Remove unwanted import

--- src/app/tag/index.js
+++ src/app/tag/index.js
@@ -1,5 +1,4 @@
 require("./tag_controller");
 require("./tag_service");
-require("./tag_form_component");
 require("./tag_modal/tag_modal_controller");
 require("./tag_new_modal/tag_new_modal_controller");
