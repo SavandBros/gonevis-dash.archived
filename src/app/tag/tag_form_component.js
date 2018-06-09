d9264c0710089b54963f0afac95e4fd60a8f93f9
Arsalan <arsalan@savandbros.com>
2018-06-09 20:50:15 +0300

    Remove tagForm component

--- src/app/tag/tag_form_component.js
+++
@@ -1,8 +0,0 @@
-import app from "../app";
-import TagFormView from './tag_form.html'
-
-function TagFormComponent() {}
-app.component("tag-form", {
-  template: TagFormView,
-  controller: TagFormComponent
-});
