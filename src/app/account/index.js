40a9b5a60755bf73764fd99c6cb05bb52f3288d1
Arsalan <arsalan@savandbros.com>
2018-07-16 15:31:20 +0300

    Import UserSiteRole class

--- src/app/account/index.js
+++ src/app/account/index.js
@@ -3,6 +3,7 @@ require('./auth/auth_interceptor_service');
 require('./auth/auth_service');
 require('./signin/signin_controller');
 require('./signup/signup_controller');
+require('./user/user_site_role');
 require('./user/user_controller');
 require('./change_pass/change_pass_controller');
 require('./email_confirmation/email_confirmation_controller');
