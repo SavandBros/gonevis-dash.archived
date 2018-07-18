a137bb792452708d63003b001af1590eff9b2df6
Arsalan <arsalan@savandbros.com>
2018-07-18 16:17:37 +0300

    Remove Tour system temporary

--- src/app/header/header_controller.js
+++ src/app/header/header_controller.js
@@ -230,15 +230,17 @@ function HeaderController($scope, $rootScope, $state, $stateParams, $timeout, Au
   /**
    * @desc Check for tours
    *
+   * @todo Fix me
+   *
    * @param {Event} event
    * @param {string} tourName
    */
-  $scope.$on("gonevisDash.Tour.readyToCheck", function (event, tourName) {
-    // Wait for DOM to finish rendering
-    $timeout(function () {
-      $scope.tour = TourService.checkForView(tourName);
-    });
-  });
+  // $scope.$on("gonevisDash.Tour.readyToCheck", function (event, tourName) {
+  //   // Wait for DOM to finish rendering
+  //   $timeout(function () {
+  //     $scope.tour = TourService.checkForView(tourName);
+  //   });
+  // });
 
   constructor();
   retrieveUser();
