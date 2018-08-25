51a848d5e8cd1dbd9aacfc3fe82bcfc53a6758a0
Arsalan Savand <arsalan@savandbros.com>
2018-08-25 21:38:27 +0400

    Add a function to open feedback modal

--- src/app/header/header_controller.js
+++ src/app/header/header_controller.js
@@ -165,6 +165,13 @@ function HeaderController($scope, $rootScope, $state, $stateParams, $timeout, Au
   };
 
   /**
+   * Open feedback modal
+   */
+  $scope.feedback = function() {
+    ModalsService.open("feedback", "FeedbackController");
+  };
+
+  /**
    * @desc Dolphin selection used for quick nevis
    *
    * @param {Event} event
