262f6feff0c4f87c89dafb13f810ddd80092ff9a
Arsalan Savand <arsalan@savandbros.com>
2018-11-11 00:05:43 +0400

    Handle a method to mutli select and delete selected items.

--- src/app/basement/multi_select/multi_select.js
+++ src/app/basement/multi_select/multi_select.js
@@ -6,41 +6,91 @@ function MultiSelect($scope, $translate) {
 
   let ctrl = this;
   let mainInput = angular.element(".select-all > input")[0];
+  let itemCount = 0;
   ctrl.countSelected = 0;
 
-  ctrl.$onInit = () => {
-    // Loop into the main list
-    angular.forEach(ctrl.master, item => {
-      // Watch each item's "isSelected" property
-      $scope.$watch(() => item.isSelected, (newValue, oldValue) => {
-        // Ignore first watch
-        if (newValue === oldValue) {
-          return;
-        }
-        // Change count
-        if (newValue) {
-          ctrl.countSelected++;
-        } else {
-          ctrl.countSelected--;
-        }
+  let items = [];
 
-        // Show minus icon
-        if (ctrl.countSelected < ctrl.master.length) {
+  /**
+   * @desc Handle selection
+   *
+   * @param {object} newValue
+   * @param {object} oldValue
+   */
+  let handleSelection = (newValue, oldValue) => {
+    // Ignore first watch
+    if (newValue === oldValue) {
+      return;
+    }
+    // Change count
+    if (newValue) {
+      ctrl.countSelected++;
+    } else {
+      ctrl.countSelected--;
+    }
+  }
+
+  /**
+   * @desc Watch item's 'isSelected' property.
+   *
+   * @param {object} item
+   */
+  let watchItems = (item) => {
+    item.listener = $scope.$watchCollection(() => [item.isSelected, item.isDeleted], (newValue, oldValue) => {
+      handleSelection(newValue[0], oldValue[0]);
+      if (newValue[1] === oldValue[1]) {
+        return;
+      }
+
+      if (newValue[1]) {
+        itemCount--;
+        mainInput.indeterminate = false;
+
+        angular.forEach(ctrl.master, item => {
+          item.isSelected = false;
+        })
+      } else {
+        itemCount++;
+        if (ctrl.isAllSelected) {
           mainInput.indeterminate = true;
-        }
-
-        // Hide minus icon and select all
-        if (ctrl.countSelected === ctrl.master.length) {
-          mainInput.indeterminate = false;
-          ctrl.isAllSelected = true;
-        }
-
-        // Show minus icon and deselect all
-        if (!ctrl.countSelected) {
-          mainInput.indeterminate = false;
           ctrl.isAllSelected = false;
         }
-      });
+      }
+    });
+    // Store items
+    items.push(item);
+  }
+
+  ctrl.$onInit = () => {
+    let initialled = true;
+    // Loop into the main list
+    angular.forEach(ctrl.master, item => {
+      watchItems(item);
+    })
+    // Watch master's new changes.
+    $scope.$watchCollection(() => ctrl.master, (newValue) => {
+      itemCount = ctrl.master.length;
+      if (initialled) {
+        initialled = false;
+        return;
+      }
+
+      // Clear all previuse listeners
+      angular.forEach(items, item => {
+        item.listener();
+      })
+
+      // Clear items array
+      items = [];
+
+      if (ctrl.isAllSelected) {
+        mainInput.indeterminate = true;
+      }
+
+      // Watch items
+      angular.forEach(newValue, (item) => {
+        watchItems(item);
+      })
     })
   };
 
@@ -68,18 +118,56 @@ function MultiSelect($scope, $translate) {
   }
 
   /**
+   * @desc Watch selection count.
+   */
+  $scope.$watch(() => ctrl.countSelected, (newValue) => {
+
+    // Show minus icon
+    if (newValue < itemCount) {
+      mainInput.indeterminate = true;
+    }
+
+    // Hide minus icon and select all
+    if (newValue === itemCount) {
+      mainInput.indeterminate = false;
+      ctrl.isAllSelected = true;
+    }
+
+    // Hide minus icon and deselect all
+    if (!newValue) {
+      mainInput.indeterminate = false;
+      ctrl.isAllSelected = false;
+    }
+  });
+
+  /**
    * @desc Select all items.
    */
   ctrl.selectAll = () => {
-    // Select all when selected count is less than master's item count
-    if (ctrl.countSelected < ctrl.master.length) {
+    if (ctrl.countSelected < itemCount) {
       ctrl.isAllSelected = true;
     }
+
     // Select all
     angular.forEach(ctrl.master, item => {
-      item.isSelected = ctrl.isAllSelected;
+      if (!item.isDeleted) {
+        item.isSelected = ctrl.isAllSelected;
+      }
     });
   }
+
+  /**
+   * @desc Load more callback
+   *
+   * @param {Event} event
+   * @param {object} data
+   */
+  $scope.$on("gonevisDash.Search:submit", function(event, data) {
+    if (data.success) {
+      ctrl.isAllSelected = false;
+      ctrl.countSelected = 0;
+    }
+  });
 }
 
 app.controller("MultiSelect", MultiSelect);
