762e7a7d0235435e9f186a04c36d7a055c06a4a6
Arsalan Savand <arsalan@savandbros.com>
2018-11-20 17:33:07 +0400

    Fix on modal close, can't scroll

--- src/app/site/payment_validation_modal/payment_validation_modal_controller.js
+++ src/app/site/payment_validation_modal/payment_validation_modal_controller.js
@@ -2,37 +2,32 @@
 
 import app from "../../app";
 
-function PaymentValidationModalController($scope, $state, $timeout, $interval, API, ModalsService, $translate, toaster, AuthService) {
+function PaymentValidationModalController($state, $timeout, $interval, API, ModalsService, $translate, toaster,
+  AuthService) {
 
   let interval;
 
   function constructor() {
     // Check if transaction is completed.
     interval = $interval(() => {
-      API.Subscription.get({ siteId: AuthService.getCurrentSite() },
-        data => {
-          if (data.subscription.active) {
-            $scope.done = true;
-            // Cancel interval
-            $interval.cancel(interval);
-            // Close modal
-            ModalsService.close("paymentValidation");
-            // Show a toaster
-            $translate(["DONE", "ACCOUNT_UPGRADED"]).then(translation => {
-              toaster.success(translation.DONE, translation.ACCOUNT_UPGRADED, 10000);
-            });
-          }
-				});
+      API.Subscription.get({ siteId: AuthService.getCurrentSite() }, data => {
+        if (data.subscription.active) {
+          // Cancel interval
+          $interval.cancel(interval);
+          // Close modal
+          ModalsService.close("paymentValidation");
+          // Redirect to main page after 500 milliseconds
+          $timeout(() => {
+            $state.go("dash.main");
+          }, 500);
+          // Show a toaster
+          $translate(["DONE", "ACCOUNT_UPGRADED"]).then(translation => {
+            toaster.success(translation.DONE, translation.ACCOUNT_UPGRADED, 10000);
+          });
+        }
+      });
     }, 2000);
   }
-
-  $scope.$watch("done", (value) => {
-    // Redirect user to main page
-    if (value) {
-      $state.go("dash.main");
-    }
-  });
-
 
   constructor();
 }
