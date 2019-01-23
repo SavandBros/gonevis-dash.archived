91c75ebd03be320b6d1580cffcd75bb11c57374c
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-01-23 19:30:43 +0400

    Add Help component

---
+++ src/app/help/help_controller.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import app from "../app";
+
+function HelpController() {
+
+  function constructor() {
+  }
+
+  constructor();
+}
+
+app.controller("HelpController", HelpController);
