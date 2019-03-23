b495d6e5102c8a9fb76055515e5ac1ce71cd7950
Amir Savand <amir@savandbros.com>
2019-03-23 17:50:31 +0430

    Dynamic forum links

--- src/app/help/help_controller.js
+++ src/app/help/help_controller.js
@@ -20,6 +20,28 @@ function HelpController($scope) {
       label: "Manage your blog",
       link: "https://support.gonevis.com/3-manage-your-blog-getting-started-on-gonevis/"
     }];
+
+    $scope.forumLinks = [{
+      label: "Homepage",
+      link: "https://forum.gonevis.com/",
+      special: true,
+    },{
+      label: "Create Thread",
+      link: "https://forum.gonevis.com/tags",
+      special: true,
+    },{
+      label: "Features",
+      link: "https://forum.gonevis.com/t/feature-requests",
+    },{
+      label: "Suggestions",
+      link: "https://forum.gonevis.com/t/suggestions",
+    },{
+      label: "Bugs",
+      link: "https://forum.gonevis.com/t/bugs",
+    },{
+      label: "In Progress",
+      link: "https://forum.gonevis.com/t/in-progress",
+    }];
   }
 
   constructor();
