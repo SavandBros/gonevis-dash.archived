4cc6ee9ee208d783be4f7d4bd00749b1f8f044bd
Amir Savand <amir@savandbros.com>
2019-03-23 16:49:30 +0430

    Add Help > GETTING STARTED section

--- src/app/help/help_controller.js
+++ src/app/help/help_controller.js
@@ -2,9 +2,24 @@
 
 import app from "../app";
 
-function HelpController() {
+function HelpController($scope) {
 
   function constructor() {
+
+    /**
+     * Links related to getting started on GoNevis
+     * @type {{link: string, label: string}[]}
+     */
+    $scope.getStartedLinks = [{
+      label: "Update your blog settings",
+      link: "https://support.gonevis.com/1-update-your-blog-settings-getting-started-on-gonevis/",
+    }, {
+      label: "Start writing",
+      link: "https://support.gonevis.com/2-start-writing-getting-started-on-gonevis/",
+    }, {
+      label: "Manage your blog",
+      link: "https://support.gonevis.com/3-manage-your-blog-getting-started-on-gonevis/"
+    }];
   }
 
   constructor();
